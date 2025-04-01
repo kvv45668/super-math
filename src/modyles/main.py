0e0e2d48ef7d44272d015c73babc9616d1ab0041
kvv45668 <pilavaleri@gmail.com>
2025-04-01 15:59:19 +0300

    test

--- src/modyles/main.py
+++ src/modyles/main.py
@@ -7,6 +7,14 @@ print(f1)
 f2 = factorial.factorial_rec(15)
 print(f2)
 
+f3 = factorial.factorial_norm(5)
+print(f3)
+
+f4 = factorial.factorial_norm(15)
+print(f4)
+
+print(f1 == f3, f2 == f4)
+
 
 p1 = perimeter.perimeter_func([1, 2, 5])
 print(p1)
