76f16c74290886f432a364d9d7372839fdbeec8f
kvv45668 <pilavaleri@gmail.com>
2025-04-01 14:02:46 +0300

    Update main.py

--- src/modyles/main.py
+++ src/modyles/main.py
@@ -1,4 +1,10 @@
 import modules.perimeter as perimetr
+import modyles.factorial as factorial
+
+f1 = factorial.factorial_norm(5)
+print(f1)
+f2 = factorial.factorial_norm(17)
+print(f2)
 
 p1 = perimeter.perimetr_func([1, 2, 5])
 print(p1)
