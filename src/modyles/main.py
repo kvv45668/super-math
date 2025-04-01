20d1e3f959594773b42d3f5dcb12c4a6e1d844cb
kvv45668 <pilavaleri@gmail.com>
2025-04-01 14:33:25 +0300

    Update main.py

--- src/modyles/main.py
+++ src/modyles/main.py
@@ -5,6 +5,13 @@ f1 = factorial.factorial_norm(5)
 print(f1)
 f2 = factorial.factorial_norm(17)
 print(f2)
+f3 =  factorial.factorial_norm(5)
+print(f3)
+f4 =  factorial.factorial_norm(15)
+print(f4)
+
+print(f1 == f3, f2 == f4)
+
 
 p1 = perimeter.perimetr_func([1, 2, 5])
 print(p1)
