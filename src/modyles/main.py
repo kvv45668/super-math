1c06cabf9733b480fbae9dee9a6be3a82297166a
kvv45668 <pilavaleri@gmail.com>
2025-04-06 16:32:49 +0300

    Update main.py

--- src/modyles/main.py
+++ src/modyles/main.py
@@ -1,5 +1,9 @@
 import modules.perimeter as perimeter
 import modules.factorial as factorial
+import modules.test as t
+import modules.tests.test_all as ta
+
+print("start 1")
 
 f1 = factorial.factorial_rec(5)
 print(f1)
@@ -18,6 +22,8 @@ print(f1 == f3, f2 == f4)
 
 p1 = perimeter.perimeter_func([1, 2, 5])
 print(p1)
+
+print("end")
 
 p2 = perimeter.perimeter_func([1, 2, 5, 7, 1])
 print(p2)
